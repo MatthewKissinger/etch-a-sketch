1245ed90d7c39265a685884f246d53e492db28ee
Matt <matthew.t.kissinger@gmail.com>
2022-02-17 14:44:14 -0800

    Finished the random color generator on hover and each cell gets 10% darker on each touch

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 // TODO List
-// 1) have each initial pass into a cell change the background color to a random RGB value
-// 2) have each subsequent pass add another 10% to the black value
-//   - after 10 passes the square should become blacked out
+
 
 // Global Variables
 
 let cellTotal = 16 * 16;
-console.log(cellTotal);
 
 // DOM Cache
 
@@ -17,15 +14,15 @@ const clearButton = document.getElementById('clearBtn');
 // Event Listeners
 
 clearButton.addEventListener('click', function() {
-    console.log('you clicked me');
     clearCanvas();
     newGridCount();
 });
 
-
 // Methods
 
 function canvasDivGenerator(cellTotal) {
+
+    sketchContainer.innerHTML = '';
 
     for (let i = 0; i < cellTotal; i++) {
         let newCell = document.createElement('div');
@@ -39,24 +36,59 @@ function canvasDivGenerator(cellTotal) {
 }
 
 function addHoverEffect(nodelist) {
+
     nodelist.forEach(node => node.addEventListener('mouseenter', function() {
-        node.style.backgroundColor = 'teal';
+
+        if (node.getAttribute('data-filled') === 'true') {
+            
+            // place all of the 'data-r' etc. values into variables
+            let valueR = node.getAttribute('data-r');
+            let valueG = node.getAttribute('data-g');
+            let valueB = node.getAttribute('data-b');
+
+            // regex that grabs the current rgb values of the cell
+            let rgb = node.style.backgroundColor;
+
+            rgb = rgb.replace(/[^\d,]/g, '').split(',');
+
+            node.style.backgroundColor = `rgb(${Math.round(rgb[0] - (valueR * .10))}, ${Math.round(rgb[1] -(valueG *.10))}, ${Math.round(rgb[2] - (valueB *.10))})`;
+            return;
+        }
+
+        node.style.backgroundColor = `rgb(${randomRGBValue()}, ${randomRGBValue()}, ${randomRGBValue()})`;
+
+        // regex that separates the initial rgb value into separate values
+        let rgb = node.style.backgroundColor;
+
+        rgb = rgb.replace(/[^\d,]/g, '').split(',');
+
+        // setting initial data attributes to be manipulated
+        // goal is to decrease each value by 10% each time the mouse hovers over an already filled cell
+        node.setAttribute('data-filled', 'true');
+        node.setAttribute('data-r', `${rgb[0]}`);
+        node.setAttribute('data-g', `${rgb[1]}`);
+        node.setAttribute('data-b', `${rgb[2]}`);
+
     }));
 }
 
 function clearCanvas() {
     let cellList = document.querySelectorAll('.cell');
-    cellList.forEach(node => node.style.backgroundColor = '');
+    cellList.forEach(node => {
+        node.style.backgroundColor = '';
+        node.removeAttribute('data-filled');
+    });
 }
 
 function newGridCount() {
     let gridRows = prompt('How many cells per row? pick between 16 and 100');
 
-    if (gridRows === null) {
+    if (gridRows === null || gridRows === '') {
         console.log('ok fine then');
-        alert('Clearing the canvas. Original row count of 16 cells being established.')
+        alert('Clearing the canvas.')
         return;
     } else {
+        console.log(gridRows);
         gridRows = parseInt(gridRows);
     }
 
@@ -69,8 +101,21 @@ function newGridCount() {
 
     cellTotal = gridRows * gridRows;
     canvasDivGenerator(cellTotal);
-
 }
 
+function randomRGBValue() {
+    return Math.floor(Math.random() * 255);
+}
+
+function decreaseBy10Percent(value) {
+    return Math.round(value - (value * .10));
+}
+
+
+// Initialize the canvas
+
 canvasDivGenerator(cellTotal);
+
+// console.log test zone
+
 
